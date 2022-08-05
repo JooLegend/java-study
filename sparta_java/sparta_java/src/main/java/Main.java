3ee16a81919d9192eba5e1243eafcaff424f6d46
Joolegend <ehenr2003@naver.com>
2022-08-05 21:48:45 +0900

    20220805 study02

--- sparta_java/sparta_java/src/main/java/Main.java
+++ sparta_java/sparta_java/src/main/java/Main.java
@@ -1,21 +1,49 @@
-import pkg.ModifierTest;
+abstract class Bird {
+    private int x, y, z;
 
-class Child extends ModifierTest{
-    void callParetProtected(){
-        System.out.println("call my parent's protected method");
-        super.messageProtected();
+    void fly(int x, int y, int z) {
+        printLocation();
+        System.out.println("이동합니다.");
+        this.x = x;
+        this.y = y;
+        if (flyable(z)) {
+            this.z = z;
+        } else {
+            System.out.println("그 높이로는 날 수 없습니다");
+        }
+        printLocation();
+    }
+
+    abstract boolean flyable(int z);
+
+    public void printLocation() {
+        System.out.println("현재 위치 (" + x + ", " + y + ", " + z + ")");
+    }
+}
+
+class Pigeon extends Bird {
+    @Override
+    boolean flyable(int z) {
+        return z < 10000;
+    }
+}
+
+class Peacock extends Bird {
+    @Override
+    boolean flyable(int z) {
+        return false;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        ModifierTest modifierTest = new ModifierTest();
-        modifierTest.messageOutside();
-//        modifierTest.messageInside();
-//        modifierTest.messageProtected();
-
-        Child child = new Child();
-        child.callParetProtected();
-
+        Bird pigeon = new Pigeon();
+        Bird peacock = new Peacock();
+        System.out.println("-- 비둘기 --");
+        pigeon.fly(1, 1, 3);
+        System.out.println("-- 공작새 --");
+        peacock.fly(1, 1, 3);
+        System.out.println("-- 비둘기 --");
+        pigeon.fly(3, 3, 30000);
     }
 }
