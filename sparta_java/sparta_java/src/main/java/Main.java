84b4ec0765fe0e4b32f55a040f4cbb526087ab4a
Joolegend <ehenr2003@naver.com>
2022-08-06 20:36:27 +0900

    20220806 study

--- sparta_java/sparta_java/src/main/java/Main.java
+++ sparta_java/sparta_java/src/main/java/Main.java
@@ -1,49 +1,29 @@
-abstract class Bird {
-    private int x, y, z;
+class Animal{
+    String name;
+    String color;
 
-    void fly(int x, int y, int z) {
-        printLocation();
-        System.out.println("이동합니다.");
-        this.x = x;
-        this.y = y;
-        if (flyable(z)) {
-            this.z = z;
-        } else {
-            System.out.println("그 높이로는 날 수 없습니다");
-        }
-        printLocation();
+    public Animal(String name) {
+        this.name = name;
     }
 
-    abstract boolean flyable(int z);
-
-    public void printLocation() {
-        System.out.println("현재 위치 (" + x + ", " + y + ", " + z + ")");
+    public void cry(){
+        System.out.println(name + "is Crying");
     }
 }
+class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
 
-class Pigeon extends Bird {
-    @Override
-    boolean flyable(int z) {
-        return z < 10000;
+    public void cry(){
+        System.out.println(name + "is Barking!!");
     }
 }
-
-class Peacock extends Bird {
-    @Override
-    boolean flyable(int z) {
-        return false;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Bird pigeon = new Pigeon();
-        Bird peacock = new Peacock();
-        System.out.println("-- 비둘기 --");
-        pigeon.fly(1, 1, 3);
-        System.out.println("-- 공작새 --");
-        peacock.fly(1, 1, 3);
-        System.out.println("-- 비둘기 --");
-        pigeon.fly(3, 3, 30000);
+        Animal dog = new Dog("코코");
+        dog.cry();
+
+
     }
 }
