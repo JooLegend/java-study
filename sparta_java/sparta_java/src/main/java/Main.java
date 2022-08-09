360b266e4ab716688cce37444be933f5498d5dce
Joolegend <ehenr2003@naver.com>
2022-08-09 23:46:52 +0900

    20220809 study

--- sparta_java/sparta_java/src/main/java/Main.java
+++ sparta_java/sparta_java/src/main/java/Main.java
@@ -1,29 +1,31 @@
-class Animal{
-    String name;
-    String color;
+class ArrayCalculation {
 
-    public Animal(String name) {
-        this.name = name;
-    }
+    int[] arr = { 0, 1, 2, 3, 4 };
 
-    public void cry(){
-        System.out.println(name + "is Crying");
+    public int divide(int denominatorIndex, int numeratorIndex) throws ArithmeticException, ArrayIndexOutOfBoundsException {
+        return arr[denominatorIndex] / arr[numeratorIndex];
     }
 }
-class Dog extends Animal{
-    public Dog(String name) {
-        super(name);
-    }
 
-    public void cry(){
-        System.out.println(name + "is Barking!!");
-    }
-}
 public class Main {
-    public static void main(String[] args) {
-        Animal dog = new Dog("코코");
-        dog.cry();
+    public static void main(String[] args) throws {
+        ArrayCalculation arrayCalculation = new ArrayCalculation();
+        try{
 
+        } catch (ArithmeticException e, ArrayIndexOutOfBoundsException e) {
 
+        }
+
+        System.out.println("2 / 1 = " + arrayCalculation.divide(2, 1));
+        try{System.out.println("1 / 0 = " + arrayCalculation.divide(1, 0));}
+        catch(Exception e){
+            System.out.println("ArithmeticException!!" + e.getMessage());
+        }// java.lang.ArithmeticException: "/ by zero"
+        try{System.out.println("Try to divide using out of index element = "
+                + arrayCalculation.divide(5, 0));}
+        catch (Exception e){
+            System.out.println("ArrayIndexOutOfBoundsException!!"+e.getMessage());// java.lang.ArrayIndexOutOfBoundsException: 5
+        }
     }
+
 }
