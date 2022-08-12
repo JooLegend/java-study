87e82db12de2cd42516c190ec1a25cd464d6332c
Joolegend <ehenr2003@naver.com>
2022-08-12 16:18:34 +0900

    20220812 study

--- sparta_java/sparta_java/src/main/java/Main.java
+++ sparta_java/sparta_java/src/main/java/Main.java
@@ -1,31 +1,16 @@
-class ArrayCalculation {
+import retrofit2.Call;
 
-    int[] arr = { 0, 1, 2, 3, 4 };
-
-    public int divide(int denominatorIndex, int numeratorIndex) throws ArithmeticException, ArrayIndexOutOfBoundsException {
-        return arr[denominatorIndex] / arr[numeratorIndex];
-    }
-}
+import java.io.IOError;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws {
-        ArrayCalculation arrayCalculation = new ArrayCalculation();
+    public static void main(String[] args) {
+        Call<Object> result = RetrofitClient.getApi().getUsers(3);
+
         try{
-
-        } catch (ArithmeticException e, ArrayIndexOutOfBoundsException e) {
-
-        }
-
-        System.out.println("2 / 1 = " + arrayCalculation.divide(2, 1));
-        try{System.out.println("1 / 0 = " + arrayCalculation.divide(1, 0));}
-        catch(Exception e){
-            System.out.println("ArithmeticException!!" + e.getMessage());
-        }// java.lang.ArithmeticException: "/ by zero"
-        try{System.out.println("Try to divide using out of index element = "
-                + arrayCalculation.divide(5, 0));}
-        catch (Exception e){
-            System.out.println("ArrayIndexOutOfBoundsException!!"+e.getMessage());// java.lang.ArrayIndexOutOfBoundsException: 5
+            System.out.println(result.execute().body());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
         }
     }
-
 }
