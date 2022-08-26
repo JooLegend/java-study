9719ab475beed88644903ff00fa9596c1d9d637c
Joolegend <ehenr2003@naver.com>
2022-08-26 20:31:28 +0900

    20220826 study

--- sparta_python/hello.py
+++ sparta_python/hello.py
@@ -1,7 +1,13 @@
-num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
-max = 0
-for num in num_list:
-    if max < num:
-        max = num
+def check_num(num):
+    pin = int(num.split('-')[1][:1])
+    if pin % 2 == 1:
+        print('남자')
+    else:
+        print('여자')
 
-print(max)
+my_pin = '123413-12413'
+my_pin2 = '123413-22413'
+my_pin3 = '123413-42413'
+check_num(my_pin)
+check_num(my_pin2)
+check_num(my_pin3)
